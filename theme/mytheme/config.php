aeced1486d57e6385b774b6832b3d54bcaf7fcc4
saurav-joshi <sauravjoshi2000@yahoo.com>
2024-04-26 15:15:58 +0800

    Update config.php

--- theme/mytheme/config.php
+++ theme/mytheme/config.php
@@ -1,1 +1,8 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
 
+$THEME->name = 'mytheme';
+$THEME->sheets = ['styles'];
+$THEME->parents = ['boost'];
+$THEME->enable_dock = false;
+$THEME->editor_sheets = [];
